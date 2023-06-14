2bc3b9f3204c49ef23a0c21f9ad2777b8d2dc5b5
root <root@LAPTOP-JQP2FJ2G.localdomain>
2023-06-14 13:49:35 -0700

    Functionality of Add Button

---
+++ engine.py
@@ -0,0 +1,61 @@
+import tkinter
+
+class Engine():
+    def __init__(self):
+        #create window
+        self.window = tkinter.Tk()
+        self.window.title("Password Manager")
+        self.window.config(padx=20, pady=20)
+        self.canvas = tkinter.Canvas(width=200, height=200)
+        #create logo
+        self.logo_img = tkinter.PhotoImage(file = "logo.png")
+        self.logo_width = self.logo_img.width()
+        self.logo_height = self.logo_img.height()
+        print(f"width = {self.logo_width}, height = {self.logo_height}")
+        self.canvas.create_image(self.logo_width/2, self.logo_height/2, image=self.logo_img)
+        self.canvas.grid(column = 1, row = 0)
+
+        #Labels
+        self.website_label = tkinter.Label(text = "Website:")
+        self.website_label.grid(column = 0, row = 1)
+        self.username_label = tkinter.Label(text = "Email/Username:")
+        self.username_label.grid(column = 0, row = 2)
+        self.password_label = tkinter.Label(text = "Password:")
+        self.password_label.grid(column = 0, row = 3)
+        
+        #Entries
+        self.website_entry = tkinter.Entry(width = 35)
+        self.website_entry.focus()
+        self.website_entry.grid(column = 1, row = 1, columnspan=2)
+        self.username_entry = tkinter.Entry(width = 35)
+        self.username_entry.insert(0,"jfrancone@sandiego.edu")
+        self.username_entry.grid(column = 1, row = 2, columnspan=2)
+        self.password_entry = tkinter.Entry(width = 17)
+        self.password_entry.grid(column = 1, row = 3)
+        
+        #Buttons
+        self.password_generator = tkinter.Button(text = "Generate Password", command = self.generate)
+        self.password_generator.grid(column = 2, row = 3)
+        self.add_button = tkinter.Button(text = "Add", command = self.add)
+        self.add_button.grid(column = 1, row = 4, columnspan = 2)
+        
+        #Attributes
+        self.website = None
+        self.username = None
+        self.password = None
+
+    #Functions
+    def run(self):
+        self.window.mainloop()
+
+    def generate(self):
+        print("Generating Password")
+
+    def add(self):
+        website = self.website_entry.get()
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        print(f"Website = {website}, Username = {username}, Password = {password}")
+        with open("data.txt", mode = "a") as file:
+            file.write(f"{website} | {username} | {password}\n")
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from tkinter import *
-import math
+from engine import Engine
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -7,43 +6,19 @@ import math
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-def generate():
-    print("Generating Password")
 
-def add():
-    print("Password Added")
+#def add(website, username, password):
+#print("Password Added")
+#add_button.website = website_entry.get()
+#add_button.username = username_entry.get()
+#add_button.password = password_entry.get()
+#print(f"Website = {website}, Username = {username}, Password = {password}")
 
-window = Tk()
-window.title("Password Manager")
-window.config(padx=20, pady=20)
-canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file = "logo.png")
-
-logo_width = logo_img.width()
-logo_height = logo_img.height()
-print(f"width = {logo_width}, height = {logo_height}")
-canvas.create_image(logo_width/2, logo_height/2, image=logo_img)
-canvas.grid(column = 1, row = 0)
-website_label = Label(text = "Website:")
-website_label.grid(column = 0, row = 1)
-website_entry = Entry(width = 35)
-website_entry.grid(column = 1, row = 1, columnspan=2)
-username_label = Label(text = "Email/Username:")
-username_label.grid(column = 0, row = 2)
-username_entry = Entry(width = 35)
-username_entry.grid(column = 1, row = 2, columnspan=2)
-password_label = Label(text = "Password:")
-password_label.grid(column = 0, row = 3)
-password_entry = Entry(width = 17)
-password_entry.grid(column = 1, row = 3)
-password_generator = Button(text = "Generate Password", command = generate)
-password_generator.grid(column = 2, row = 3)
-add_button = Button(text = "Add", command = add)
-add_button.grid(column = 1, row = 4, columnspan = 2)
 
 
 #user_width = math.floor(website_entry['width']/2)
 #print(user_width)
 
 #print("Entering Main Loop...")
-window.mainloop()
+engine = Engine()
+engine.run()
